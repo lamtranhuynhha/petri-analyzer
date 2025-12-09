1059d601152a619879532351ef0cebc3cf4917fe
Tran Huynh Ha Lam <tranhuynhhalam4c@gmail.com>
2025-12-10 01:35:53 +0700

    [fix] API url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://127.0.0.1:8000/api';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://petrinet-analyzer.onrender.com/api';
 
 // Create axios instance with default config
 const apiClient = axios.create({
